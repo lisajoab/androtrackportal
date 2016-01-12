0398a4b5302c1aee9dbcdb6748b14cca2bb5cf87
fmarube <fredndk@gmail.com>
2016-01-12 12:55:08 +0300

    Initial pom commit

---
+++ src/main/java/com/androtrackportal/AndrotrackportalApplication.java
@@ -0,0 +1,12 @@
+package com.androtrackportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndrotrackportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndrotrackportalApplication.class, args);
+	}
+}
